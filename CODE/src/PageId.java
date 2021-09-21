fb0e2882a5c706004c02d20c74cdf0ebf2c15a4d
Shuitaa <pierre.averty2001@gmail.com>
2021-09-21 10:38:06 +0200

    Initialization of PageId class and write of 2 variables

---
+++ CODE/src/PageId.java
@@ -0,0 +1,10 @@
+// Class that manage page's id.
+
+public class PageId{
+    
+    // Properties of the PageId class.
+
+    int FileIdx; // Id of the file which contains the current page.
+    int PageIdx; // Id of the current page.
+
+}
