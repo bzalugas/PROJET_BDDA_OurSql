df4b7b761d983922dde1b7044e9c76228936e02c
Shuitaa <pierre.averty2001@gmail.com>
2021-09-21 11:36:08 +0200

    Adding the constructor of the PageId class, the equals method and fixing the toString method.

--- CODE/src/PageId.java
+++ CODE/src/PageId.java
@@ -7,4 +7,41 @@ public class PageId{
     private int FileIdx; // Id of the file which contains the current page.
     private int PageIdx; // Id of the current page.
 
+    // Constructor of the PageId class.
+    public PageId (int FileIdx, int PageIdx){
+        this.FileIdx = FileIdx;
+        this.PageIdx = PageIdx;
+    }
+
+    // Stringify page id.
+    public String toString(){
+        return "(" + FileIdx + "," + PageIdx + ")";
+    }
+
+    // Test if two page id are equals.
+    public boolean equals(PageId obj){
+        return obj.getFileIdx() == this.FileIdx
+            && obj.getPageIdx() == this.PageIdx;
+    }
+
+
+    // Gets FileIdx.
+    public int getFileIdx(){
+        return FileIdx;
+    }
+
+    // Gets FileIdx.
+    public int getPageIdx(){
+        return PageIdx;
+    }
+
+    // Set file id.
+    public void setFileIdx(int FileIdx){
+        this.FileIdx = FileIdx;
+    }
+
+    // Set page id.
+    public void setPageIdx(int PageIdx){
+        this.PageIdx = PageIdx;
+    }
 }
