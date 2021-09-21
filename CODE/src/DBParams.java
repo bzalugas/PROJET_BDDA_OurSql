5cb5ad5dde4058d7091e47d66d961c035c1f42d3
Crusher92 <ganakevin@yahoo.fr>
2021-09-21 11:16:17 +0200

    Update DBParams.java
    Ajout de description

--- CODE/src/DBParams.java
+++ CODE/src/DBParams.java
@@ -1,6 +1,7 @@
+//Class that give us the DB settings
 public class DBParams {
-	public static String DBpath;
-	public static int pageSize;
-	public static int maxPagesPerFile;
+	public static String DBpath;// Data Base path
+	public static int pageSize;// Size of pages
+	public static int maxPagesPerFile;// Maximum of pages allowed per file
 
 }
