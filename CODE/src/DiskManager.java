459dfa3e5ff7f492a4d862dc42d54d9dc54fb31a
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-09-22 18:35:35 +0200

    Correct errors from DiskManager & start implementing allocPage() method

--- CODE/src/DiskManager.java
+++ CODE/src/DiskManager.java
@@ -1,8 +1,12 @@
+import java.nio.ByteBuffer;
+import java.io.File;
 // class to manage disk operations (Singleton)
 public class DiskManager
 {
     //Single instance of DiskManager
-    private static final DiskManager INSTANCE = null;
+    private static DiskManager instance = null;
+    //registry of pages (singleton)
+    private Registry reg = Registry.getInstance();
 
     private DiskManager(){
         //Private constructor to avoid default
@@ -10,12 +14,21 @@ public class DiskManager
 
     //getter for instance
     public static final DiskManager getInstance(){
-        if (INSTANCE == null)
-            INSTANCE = new DiskManager();
-        return INSTANCE;
+        if (instance == null)
+            instance = new DiskManager();
+        return instance;
     }
 
     public PageId AllocPage(){
+        int[] page = reg.pageAvailable();
+        if (page[0] == -1)
+        {
+            String pathFile = DBParams.DBPath + "F" + Integer.toString(reg.getLastFileIndex()) + ".df";
+            File f = new File(pathFile);
+        }
+            //Create file -> add file & pages to reg -> page=reg.pageAvailable();
+            PageId pi = new PageId(page[0], page[1]);
+        return (pi);
 
     }
 
