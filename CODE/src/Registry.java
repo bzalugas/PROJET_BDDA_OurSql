d443ab0d4e747c40511a97a1b7d0abf4b67905eb
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-09-22 18:34:23 +0200

    Correct errors from class Registry & add lastFileIndex

--- CODE/src/Registry.java
+++ CODE/src/Registry.java
@@ -1,18 +1,36 @@
 import java.util.ArrayList;
+import java.io.Serializable;
 
 public class Registry implements Serializable {
 
-    private static Singleton instance;
-    ArrayList<DataFile> files = new DataFile();
+    private static Registry instance = null;
+    private ArrayList<DataFile> files = new ArrayList<DataFile>();
+    private int lastFileIndex = 0;
 
-    private Singleton(){
+    private Registry(){
         
     }
 
-    public static final Singleton getInstance(){
+    public static final Registry getInstance(){
         if (instance == null)
-            instance = new Singleton();
+            instance = new Registry();
         return instance;
     }
 
+    public void newFile(){
+        //add new DataFile to ArrayList (increment lastFileIndex)
+    }
+
+    public int[] pageAvailable() {
+        int [] page = {-1, -1};
+
+        for (DataFile file : files)
+            if ((page[1] = file.pageAvailable()) != -1)
+                page[0] = file.getFileIndex();
+        return (page);
+    }
+
+    public int getLastFileIndex(){
+        return (lastFileIndex);
+    }
 }
