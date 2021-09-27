3acb8c87623f95ff195620447c4ce64e6db987ff
Shuitaa <pierre.averty2001@gmail.com>
2021-09-27 11:14:49 +0200

    Adding the save() method to the registry class.

--- CODE/src/Main.java
+++ CODE/src/Main.java
@@ -1,21 +1,37 @@
-// Main class of the project.
+
+Main class of the project.
 
 public class Main{
 
     public static void main(String[] args){
+        
         DBParams.DBPath = args[0];
+        
         DBParams.pageSize = 4096;
+        
         DBParams.maxPagesPerFile = 4;
+        
         try {
+        
             DiskManager dm = DiskManager.getInstance();
+        
             PageId page;
+        
             for (int i = 0; i < 23; i++){
+        
                 page = dm.AllocPage();
+        
                 System.out.println("page created : " + page.toString());
+        
             }
+        
         } catch(Exception e){
+        
             System.out.println("exception : " + e.getMessage());
+        
         }
+
+
     }
 
 }
--- CODE/src/Registry.java
+++ CODE/src/Registry.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
 import java.io.Serializable;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.File;
+import java.io.IOException;
+
 
 public class Registry implements Serializable {
 
     private static Registry instance = null;
     private ArrayList<DataFile> files = new ArrayList<DataFile>();
     private int lastFileIndex = -1;
-
+    
     private Registry(){
-        
+
     }
 
     public static final Registry getInstance(){
@@ -20,6 +27,17 @@ public class Registry implements Serializable {
     //method to add new DataFile in the list
     public void newFile(String filePath){
         files.add(new DataFile(++lastFileIndex, filePath));
+    }
+
+    public void save () throws IOException{
+            File file  = new File("./DB/registry.reg");
+            FileOutputStream fileOut = new FileOutputStream(file);
+            ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+            objOut.writeObject(this);
+            objOut.flush();
+            objOut.close();
+            fileOut.close();
+
     }
 
     public int[] pageAvailable() {
