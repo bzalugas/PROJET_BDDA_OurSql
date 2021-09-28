e8c48723a673e605c625b604d2dde593e8145ce0
Crusher92 <ganakevin@yahoo.fr>
2021-09-28 12:12:18 +0200

    Update BufferManager.java

--- CODE/src/BufferManager.java
+++ CODE/src/BufferManager.java
@@ -2,7 +2,8 @@
 import java.util.ArrayList;
 
 public class BufferManager {
-	ArrayList<Frame> bufferPool;
+	private ArrayList<Frame> bufferPool;
+	private DiskManager dmanager = DiskManager.getInstance();
 
 	public BufferManager(){
 
@@ -10,10 +11,21 @@ public class BufferManager {
 
 	public void freePage(PageId pageId, boolean valDirty) {
 		for(Frame frame : this.bufferPool) {
-			if(frame.pageId == pageId) {
-				frame.pinCount--;
-				frame.flagDirty = valDirty;
+			if(frame.getPageId() == pageId) {
+				frame.setPinCount(frame.getPinCount()-1);
+				frame.setFlagDirty(valDirty);
 			}
 		}
+	}
+
+	public ByteBuffer getPage(PageId pageId) {
+		 ByteBuffer buff = new ByteBuffer();
+		 buff.allocate(DBParams.pageSize);
+		 dmanager.readPage(pageId,buff);
+
+		 Frame f = new Frame(pageId,buff);
+		 f.setPinCount(f.getPinCount()+1);
+
+		 return buff;
 	}
 }
