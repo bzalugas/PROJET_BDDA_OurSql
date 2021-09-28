6c5461bdd7d70a1aa341c07cb04459ed5a255b16
Shuitaa <pierre.averty2001@gmail.com>
2021-09-28 10:55:50 +0200

    Adding the bufferPool property

--- CODE/src/BufferManager.java
+++ CODE/src/BufferManager.java
@@ -1,7 +1,15 @@
 //Class to manage frames
+import java.util.ArrayList;
+
 public class BufferManager {
+	ArrayList<Frame> bufferPool;
 
 	public BufferManager(){
+
+	}
+
+
+	public void freePage(PageId pageId, byte valDirty) {
 
 	}
 
