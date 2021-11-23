ff00e1e8b42d33ec80248195df783e738a1b5e7a
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-11-23 12:10:53 +0100

    Update record

--- CODE/src/kernel/Record.java
+++ CODE/src/kernel/Record.java
@@ -14,63 +14,84 @@ public class Record
         this.values = new ArrayList<String>();
     }
 
+    /**
+     * Method to write values to buffer from a position
+     * @param buff the buffer
+     * @param pos the starting position
+     */
     public void writeToBuffer(ByteBuffer buff, int pos)
     {
+        int     i;
+        int     j;
+        String  type;
+        int     len;
+
         buff.position(pos);
-        for(String value : this.values)
+        for (i = 0; i < values.size(); i++)
         {
-            for(String type : this.relInfo.getColumnType())
+            type = relInfo.getColumnType()[i].toLowerCase();
+            switch(type)
             {
-                switch(type)
-                {
-                    case "int":
-                        int resInt = Integer.valueOf(value);
-                        buff.putInt(resInt);
-                        break;
-                    case "float":
-                        float resFloat = Float.valueOf(value);
-                        buff.putFloat(resFloat);
-                        break;
-                    default:
-                        int len = value.length();
-                        for (int i = 0; i < len; i++)
-                            buff.putChar(value.charAt(i));
-            
-                        break;
-                }
+                case "int":
+                    int resInt = Integer.valueOf(values.get(i));
+                    buff.putInt(resInt);
+                    break;
+                case "float":
+                    float resFloat = Float.valueOf(values.get(i));
+                    buff.putFloat(resFloat);
+                    break;
+                default:
+                    if (type.substring(0,5).equals("string"))
+                    {
+                        len = Integer.parseInt(type.substring(6));
+                        for (j = 0; j < len; j++)
+                            buff.putChar(values.get(i).charAt(i));
+                    }
+                    break;
             }
         }
     }
 
-    public void readFromBuffer(ByteBuffer buff, int pos){
+    public void readFromBuffer(ByteBuffer buff, int pos)
+    {
+        int     resInt;
+        float   resFloat;
+        int     len;
+        int     i;
+        int     j;
+        char[]  chars;
+        String  resString;
+
         this.values.clear();
-            for(String type : this.relInfo.getColumnType())
+        buff.position(pos);
+        for(String type : this.relInfo.getColumnType())
+        {
+            switch(type)
             {
-                switch(type)
-                {
-                    case "int":
-                        int resInt = buff.getInt(pos);
-                        this.values.add(String.valueOf(resInt));
-                        break;
-                    case "float":
-                        float resFloat = buff.getFloat(pos);
-                        this.values.add(String.valueOf(resFloat));
-                        break;
-                    default:
-                        int len = Integer.parseInt(type.substring(6));
-                        int j = 0;
-                        char [] chars = new char[len];
-                        for (int i = pos; i < pos+len; i++){
+                case "int":
+                    resInt = buff.getInt();
+                    this.values.add(String.valueOf(resInt));
+                    break;
+                case "float":
+                    resFloat = buff.getFloat();
+                    this.values.add(String.valueOf(resFloat));
+                    break;
+                default:
+                    if (type.substring(0,5).equals("string"))
+                    {
+                        len = Integer.parseInt(type.substring(6));
+                        j = 0;
+                        chars = new char[len];
+                        for (i = pos; i < pos+len; i++){
                             chars[j] = buff.getChar(i);
                             j++;
                         }
-                        
-                        String resString = new String(chars);
-                        this.values.add(resString+len);
-
-                        break;
-                }
+                        resString = new String(chars);
+                        this.values.add(resString);
+                    }
+                    break;
             }
+        }
     }
 
     public ArrayList<String> getValues()
