c09c89424f9c3cb9d278be34582d47519b4bc131
Crusher92 <ganakevin@yahoo.fr>
2021-12-10 00:56:52 +0100

    ajout commande terminal

--- oursql.sh
+++ oursql.sh
@@ -1,2 +1,10 @@
 javac -d CODE/bin -cp CODE/src CODE/src/*.java
 java -cp CODE/bin Main ./DB
+
+
+##Windows
+javac -d CODE/bin -cp CODE/src CODE/src/*.java
+javac -d CODE/bin -cp CODE/src CODE/src/kernel/*.java
+javac -d CODE/bin -cp CODE/src CODE/src/kernel/exceptions/*.java
+
+java -cp CODE/bin **** ./DB
