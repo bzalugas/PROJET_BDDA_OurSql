d53de9aac5502534a116afee2f76ed7200baa592
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-12-13 07:00:26 +0100

    Update oursql.sh

--- oursql.sh
+++ oursql.sh
@@ -1,10 +1,15 @@
-javac -d CODE/bin -cp CODE/src CODE/src/*.java
+find ./CODE/src/kernel -name "*.java" > sources.txt
+# find ./CODE/src/ker -name "*.java" >> sources.txt
+echo ./CODE/src/Main.java >> sources.txt
+# CODE/src/kernel/*.java CODE/src/kernel/exceptions/*.java
+javac -d CODE/bin -cp CODE/src @sources.txt
 java -cp CODE/bin Main ./DB
+rm sources.txt
 
 
 ##Windows
-javac -d CODE/bin -cp CODE/src CODE/src/*.java
-javac -d CODE/bin -cp CODE/src CODE/src/kernel/*.java
-javac -d CODE/bin -cp CODE/src CODE/src/kernel/exceptions/*.java
+# javac -d CODE/bin -cp CODE/src CODE/src/*.java
+# javac -d CODE/bin -cp CODE/src CODE/src/kernel/*.java
+# javac -d CODE/bin -cp CODE/src CODE/src/kernel/exceptions/*.java
 
-java -cp CODE/bin **** ./DB
+# java -cp CODE/bin **** ./DB
